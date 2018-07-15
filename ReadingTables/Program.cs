311d8d98a3a2e8e7a6505a51472ad9d7317b4bf3
lukebrandonfrancis <lukebrandonfrancis@gmail.com>
2018-07-15 18:43:03 -0400

    - Created custom methods to work with tables

--- ReadingTables/Program.cs
+++ ReadingTables/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
 
 namespace ReadingTables
 {
-    class Program
+    class Program : Base 
     {
         static void Main(string[] args)
         {
+            Driver = new ChromeDriver();
+
+            Driver.Navigate().GoToUrl("file:///C:/TablePages/SimpleTable.html");
+
+            TablePage page = new TablePage();
+
+            // Read table   
+            Utilities.ReadTable(page.table);
+
+            Console.WriteLine("-----------------------------------");
+
+            // Get the cell value from the table
+            Console.WriteLine("The Name {0} with Email {1} and Phone {2}",
+                Utilities.ReadCell("Name", 2), Utilities.ReadCell("Email", 2), Utilities.ReadCell("Phone", 2));
+
+            Console.Read();
         }
     }
 }
---
+++ ReadingTables/Utilities.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace ReadingTables
+{
+    class Utilities
+    {
+        static List<TableDataCollection> _tableDataCollection = new List<TableDataCollection>();
+
+        public static void ReadTable(IWebElement table)
+        {
+            // Get all the columns for the table 
+            var columns = table.FindElements(By.TagName("th"));
+
+            // Get all the rows
+            var rows = table.FindElements(By.TagName("tr"));
+
+            // Create row index
+            int rowIndex = 0;
+
+            foreach (var row in rows)
+            {
+                int colIndex = 0;
+
+                var colDatas = row.FindElements(By.TagName("td"));
+
+                foreach (var colValue in colDatas)
+                {
+                    _tableDataCollection.Add(new TableDataCollection
+                    {
+                        RowNumber = rowIndex,
+                        ColumnName = columns[colIndex].Text,
+                        ColumnValue =  colValue.Text
+                    });
+                    
+                    // Move to next column 
+                    colIndex++;
+                }
+
+                rowIndex++;
+            }
+        }
+
+        public static string ReadCell(string columnName, int rowNumber)
+        {
+            var data = (from e in _tableDataCollection
+                where e.ColumnName == columnName && e.RowNumber == rowNumber
+                select e.ColumnValue).SingleOrDefault();
+            return data;
+        }
+    }
+
+    public class TableDataCollection
+    {
+        public int RowNumber { get; set; }
+        public string ColumnName { get; set; }
+        public string ColumnValue { get; set; }
+    }
+}
